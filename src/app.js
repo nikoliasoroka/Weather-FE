9d413d3fb8d155a43f884e788e2f8394be139931
nikoliasoroka <nikoliasoroka@gmail.com>
2022-08-06 20:27:20 +0300

    add forecast js

--- src/app.js
+++ src/app.js
@@ -24,6 +24,32 @@ function displayDate(timestamp) {
   return `${days[date.getDay()]} ${hours}:${minutes}`;
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
+  let forecastHTML = "";
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col">
+              <div class="weather-forecast-date">${day}</div>
+              <img
+                src="http://openweathermap.org/img/wn/02n@2x.png"
+                alt=""
+                width="36"
+              />
+              <div class="weather-forecast-temperatures">
+                <span class="weather-forecast-temperature-max">18° </span
+                ><span class="weather-forecast-temperature-min">12° </span>
+              </div>
+            </div>
+            `;
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -87,3 +113,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
+displayForecast();
