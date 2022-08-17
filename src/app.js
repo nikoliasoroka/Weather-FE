ca2583bc16a994f2a9330592ca6dc91038724922
MSorokaIsraelIT <mykola.s@israelit.pro>
2022-08-17 15:16:37 +0300

    make responsive

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function displayForecast(response) {
   days.forEach(function (day, index) {
     if (index < 5) {
       forecastHTML += `
-      <div>
+      <div class="col-sm mb-3">
               <div class="weather-forecast-date">${returnDay(
                 day.dt * 1000
               )}</div>
